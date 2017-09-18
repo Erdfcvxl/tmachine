e99db8676c5bbadc280a9e751e6392971a73c2c4
rokas <rokasr788@gmail.com>
2017-09-17 20:36:18 -0400

    working without file read functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {Turing} from './services/turing';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./styles/primary-grid.css']
 })
 export class AppComponent {
     _headPosition: any;
@@ -18,98 +17,34 @@ export class AppComponent {
             this._orders !== null
         ) {
             new Turing(this._headPosition, this._lineInput, this._orders);
+            document.querySelectorAll('.primary-grid')[0].classList.add('primary-grid---proccess');
         }
     }
 
-    getOrdersFromString(string) {
-        let orders = string.split('\n');
-
-        orders = orders.filter(function(e){ return e});
-
-        console.log(orders);
+    expand() {
+        document.querySelectorAll('.primary-grid')[0].classList.remove('primary-grid---proccess');
+        location.reload();
     }
 
+
     populateDummy() {
-        this._headPosition = 1;
-        this._lineInput = '00V0000000001111*A000000000001111*B000000000000000000000000ZR000000000H000000';
-        this._orders = '0 0 0 R 0\n' +
-            '0 1 1 R 0\n' +
-            '0 V V R 0 \n' +
-            '0 * * L 1\n' +
-            '\n' +
-            '1 0 * R 2\n' +
-            '1 1 * R 3\n' +
-            '1 V V R 20\n' +
-            '1 V V R 20\n' +
-            '\n' +
-            '2 * 0 R 4\n' +
-            '3 * 1 R 5\n' +
-            '\n' +
-            '4 A A R 4\n' +
-            '4 0 0 R 4\n' +
-            '4 1 1 R 4\n' +
-            '5 A A R 5\n' +
-            '5 0 0 R 5\n' +
-            '5 1 1 R 5\n' +
-            '\n' +
-            '4 * * L 6\n' +
-            '5 * * L 7\n' +
-            '\n' +
-            '6 0 * R 8\n' +
-            '6 1 * R 9\n' +
-            '7 0 * R 10\n' +
-            '7 1 * R 11\n' +
-            '6 A A R 20\n' +
-            '7 A A R 20\n' +
-            '\n' +
-            '8 * 0 R 12\n' +
-            '9 * 1 R 13\n' +
-            '10 * 0 R 14\n' +
-            '11 * 1 R 15\n' +
-            '\n' +
-            '12 B B R 12\n' +
-            '12 0 0 R 12\n' +
-            '12 1 1 R 12\n' +
-            '13 B B R 13\n' +
-            '13 0 0 R 13\n' +
-            '13 1 1 R 13\n' +
-            '14 B B R 14\n' +
-            '14 0 0 R 14\n' +
-            '14 1 1 R 14\n' +
-            '15 B B R 15\n' +
-            '15 0 0 R 15\n' +
-            '15 1 1 R 15\n' +
-            '\n' +
-            '12 Z 0 L 16\n' +
-            '13 Z 1 L 16\n' +
-            '14 Z 1 L 16\n' +
-            '15 Z 0 L 17\n' +
-            '\n' +
-            '12 C 1 L 16\n' +
-            '13 C 0 L 17\n' +
-            '14 C 0 L 17\n' +
-            '15 C 1 L 17\n' +
-            '\n' +
-            '16 0 Z L 18\n' +
-            '17 0 C L 18\n' +
-            '\n' +
-            '18 A A L 18\n' +
-            '18 B B L 18\n' +
-            '18 * * L 18\n' +
-            '18 0 0 L 18\n' +
-            '18 1 1 L 18\n' +
-            '18 V V R 0\n' +
-            '\n' +
-            '20 A A R 20\n' +
-            '20 B B R 20\n' +
-            '20 * * R 20\n' +
-            '20 0 0 R 20\n' +
-            '20 1 1 R 20\n' +
-            '20 Z 0 R 21\n' +
-            '20 C 1 R 21\n' +
-            '\n' +
-            '21 0 0 R X\n' +
-            '21 1 1 R X\n' +
-            '\n';
+        this._headPosition = 7;
+        this._lineInput = '000100000000000000001111111000000000000000010000';
+        this._orders = `0 0 0 R 0
+0 1 0 R 11
+11 1 1 R 11
+11 0 1 R 2
+2 0 0 L 3
+2 1 1 L 4
+3 1 1 L 3
+3 0 0 R 0
+4 1 0 L 5
+4 0 0 L 4
+5 1 1 L 5
+5 0 1 L 12
+12 0 0 R 6
+12 1 1 R 0
+6 1 1 R 6
+6 0 0 L 4`;
     }
 }
--- src/app/services/turing.ts
+++ src/app/services/turing.ts
@@ -1,21 +1,146 @@
 export class Turing {
-    _headPosition: null;
-    _lineInput: null;
-    _orders: null;
+    headPosition: number;
+    _line: Array<any> = [];
+    _orders: object;
 
     constructor(headPosition, lineInput, orders) {
-        this._headPosition = headPosition;
-        this._lineInput = lineInput;
-        this._orders = this.getOrdersFromString(orders);
-
+        this.headPosition = headPosition - 1;
+        this.getLineFromString(lineInput);
+        this.getOrdersFromString(orders);
         this.init();
     }
 
     getOrdersFromString(string) {
-        this._orders = string.split('\n').filter(function(e){ return e});
+        const linesArray = string.split('\n').filter(function (e) {
+            return e;
+        });
+
+        const result = {};
+
+
+        for (let i = 0; i < linesArray.length; i++) {
+            const state = linesArray[i].split(' ')[0];
+
+            if (typeof result[state] !== 'undefined') {
+                result[state].push(this.getOrderJSON(linesArray[i]));
+            } else {
+                result[state] = [this.getOrderJSON(linesArray[i])];
+            }
+        }
+
+
+        this._orders = result;
+    }
+
+    getLineFromString(string) {
+        for (let i = 0; i < string.length; i++) {
+            this._line.push(string[i]);
+        }
     }
 
     init() {
+        this.printLine();
+        this.proceed(0);
+    }
 
+    proceed(state, ms = 10) {
+        let i = 0;
+        const self = this;
+        const orderGroup = this._orders[state];
+        const stopBefore = orderGroup.length;
+        const step = function (delay = null) {
+            if (i < stopBefore) {
+                i++;
+                if (delay) {
+                    setTimeout(iteration, delay);
+                } else {
+                    iteration();
+                }
+            }
+        };
+        const iteration = function () {
+            const order = orderGroup[i];
+
+            if (typeof order === 'undefined') {
+                console.log(i);
+                return 0;
+            }
+
+            if (self.validateSymbols(order.currentSymbol)) {
+                if (self.setCurrentSymbol(order.newSymbol)) {
+                    self.printLine();
+                }
+
+                self.moveHead(order.direction);
+
+                self.proceed(order.newState, 10);
+                return 0;
+            }
+
+            step(0);
+        };
+
+        setTimeout(iteration, ms);
+    }
+
+    clearHistory() {
+        const historyEl = document.querySelectorAll('.primary-grid--history')[0];
+
+        while (historyEl.hasChildNodes() && historyEl.childElementCount > 80) {
+            historyEl.removeChild(historyEl.firstChild);
+        }
+    }
+
+    printLine() {
+        const historyEl = document.querySelectorAll('.primary-grid--history')[0];
+        const node = document.createElement('div');
+        node.innerHTML = this._line.join('');
+
+        if (historyEl.childElementCount > 100) {
+            this.clearHistory();
+        }
+        historyEl.appendChild(node);
+        historyEl.scrollTo(0, historyEl.scrollHeight);
+    }
+
+    /* Helpers */
+    validateSymbols(symbol) {
+        return this.getCurrentSymbol() == symbol || symbol === '*';
+    }
+
+    getOrderJSON(order) {
+        order = order.split(' ');
+
+        return {
+            currentSymbol: order[1],
+            newSymbol: order[2],
+            direction: order[3],
+            newState: order[4],
+        };
+    }
+
+    getCurrentSymbol() {
+        return this._line[this.headPosition];
+    }
+
+    setCurrentSymbol(symbol) {
+        if (symbol !== '*' && this._line[this.headPosition] !== symbol) {
+            return this._line[this.headPosition] = symbol;
+        }
+
+        return false;
+    }
+
+    moveHead(direction) {
+        switch (direction) {
+            case 'R':
+                this.headPosition++;
+                break;
+            case 'L':
+                this.headPosition--;
+                break;
+            case '*':
+                break;
+        }
     }
 }
